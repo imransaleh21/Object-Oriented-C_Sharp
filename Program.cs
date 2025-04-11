bae128b0446c2bfae150814b8bb41fa697fc8367
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-04-12 01:31:12 +0600

    Learn static constractor and its calling by different ways

--- Program.cs
+++ Program.cs
@@ -34,6 +34,8 @@ namespace C__Oops
             Obj3.sum(1,2,3,4,5,6,7,8,9);
             Console.WriteLine();
 
+            new Access_Static_Constractor(); // call the constractor of the class
+
             Console.ReadLine();
         }
 
---
+++ Static Constractor Calling with other Constractor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Oops
+{
+    internal class Static_Constractor_Call
+    {
+        private static int  number;  
+        internal Static_Constractor_Call()
+        {
+            Console.WriteLine("Number: "+ number);
+            number = 5;
+        }
+        static Static_Constractor_Call() // this static constractor will call first
+        {
+            Console.WriteLine("This static constractor will run always");
+            number = 15;
+        }
+        public static void numberShow()
+        {
+            Console.WriteLine("Static Constractor Number value Change: "+ number);
+        }
+    }
+
+    internal class Access_Static_Constractor
+    {
+        internal Access_Static_Constractor()
+        {
+            //Static_Constractor_Call SCC = new Static_Constractor_Call();
+            // if we do not create any object then only the value of static constractor will assign
+            Static_Constractor_Call.numberShow(); 
+        }
+        
+    }
+
+}
